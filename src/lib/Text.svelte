e7a9a05541885318a0250ec71263839dbf4840f9
Carl Törnqvist <carl@tornqv.ist>
2023-09-26 13:44:06 +0200

    Handle lists

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -11,12 +11,36 @@
 <script>
   export let content = null
   export let plain = false
+
+  $: blocks = Array.isArray(content)
+    ? content.reduce(function listify(acc, block) {
+        const last = acc.at(-1)
+        if (block.listItem) {
+          const { _key, level } = block
+          const _type = block.listItem === 'bullet' ? '$ul' : '$ol'
+          const children = block.children?.reduce(listify, [])
+          const item = { children, _key, _type: '$li' }
+          if (last._type === _type) {
+            last.children.push(item)
+          } else {
+            acc.push({ _key, _type, level, children: [item] })
+          }
+        } else {
+          if (block.children) {
+            block = { ...block, children: block.children.reduce(listify, []) }
+          }
+          acc.push(block)
+        }
+        return acc
+      }, [])
+    : []
+  $: console.log(blocks)
 </script>
 
 {#if plain}
   {@html asText(content).replace(/\n/g, '<br />')}
 {:else if content}
-  {#each content as block (block._key)}
+  {#each blocks as block (block._key)}
     {@const { style, _type } = block}
     <slot {block}>
       {#if style === 'normal'}
@@ -35,6 +59,16 @@
         <h6><svelte:self content={block.children} /></h6>
       {:else if style === 'blockquote'}
         <blockquote><svelte:self content={block.children} /></blockquote>
+      {:else if _type === '$ul'}
+        <ul>
+          <svelte:self content={block.children} />
+        </ul>
+      {:else if _type === '$ol'}
+        <ol>
+          <svelte:self content={block.children} />
+        </ol>
+      {:else if _type === '$li'}
+        <li><svelte:self content={block.children} /></li>
       {:else if _type === 'span'}
         {@html block.marks.reduce(
           (acc, mark) => `<${mark}>${acc.replace(/\n/g, '<br />')}</${mark}>`,
