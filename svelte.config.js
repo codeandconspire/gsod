5502ebb80d6504bb1e4ede8a70e6e10f9fca75e6
Carl Törnqvist <carl@tornqv.ist>
2023-10-16 10:17:25 +0200

    Fix whitespace in links

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare'
+import preprocess from 'svelte-preprocess'
+
+const tagsRegex1 = /(>)[\s]*([<{])/g
+const tagsRegex2 = /({[/:][a-z]+})[\s]*([<{])/g
+const tagsRegex3 = /({[#:][a-z]+ .+?})[\s]*([<{])/g
+const tagsRegex4 = /([>}])[\s]+(<|{[/#:][a-z][^}]*})/g
+const tagsReplace = '$1$2'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +14,17 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter()
-  }
+  },
+  preprocess: preprocess({
+    replace: [
+      // Remove whitespace between tags
+      // https://github.com/sveltejs/svelte/issues/189#issuecomment-1376746165
+      [tagsRegex1, tagsReplace],
+      [tagsRegex2, tagsReplace],
+      [tagsRegex3, tagsReplace],
+      [tagsRegex4, tagsReplace]
+    ]
+  })
 }
 
 export default config
